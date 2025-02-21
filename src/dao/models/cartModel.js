4baa72a5db3fa7807da00fab19448385ffa79b69
unknown <ricardo.gp2007@gmail.com>
2025-02-21 16:57:47 -0400

    EntregaFinal8

--- src/dao/models/cartModel.js
+++ src/dao/models/cartModel.js
@@ -3,6 +3,11 @@ import mongoose from 'mongoose';
 const cartCollection = 'carts';
 
 const cartSchema = new mongoose.Schema({
+    user: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User',        // Asegúrate que el modelo de usuario se registre con el nombre "User"
+        required: true 
+      },
     products: [
         {
             product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
