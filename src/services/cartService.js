c06c1f7834668a915ee1de4218a17f46c1d351c4
unknown <ricardo.gp2007@gmail.com>
2025-02-21 15:52:18 -0400

    EntregaFinal6

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -2,7 +2,9 @@ import { cartDBManager } from '../dao/cartDBManager.js';
 import { productDBManager } from '../dao/productDBManager.js';
 import { ticketDBManager } from '../dao/ticketDBManager.js';
 
-const CartService = new cartDBManager(new productDBManager());
+const productManager = new productDBManager();
+
+const CartService = new cartDBManager(productManager);
 const TicketService = new ticketDBManager();
 
 export const getCartById = async (cid) => {
@@ -12,33 +14,29 @@ export const getCartById = async (cid) => {
 export const addProductToCart = async (cid, pid) => {
     return await CartService.addProductByID(cid, pid);
 };
-
+  
 export const purchaseCart = async (cid) => {
     const cart = await CartService.getProductsFromCartByID(cid);
     const insufficientStock = [];
     let totalAmount = 0;
-
     for (const item of cart.products) {
-        const product = await productDBManager.getProductByID(item.product._id);
-        if (product.stock < item.quantity) {
-            insufficientStock.push(product._id);
-        } else {
-            totalAmount += product.price * item.quantity;
-            await productDBManager.updateProduct(item.product._id, { stock: product.stock - item.quantity });
-        }
+      // Usa la instancia productManager para llamar a los métodos
+      const product = await productManager.getProductByID(item.product._id);
+      if (product.stock < item.quantity) {
+        insufficientStock.push(product._id);
+      } else {
+        totalAmount += product.price * item.quantity;
+        await productManager.updateProduct(item.product._id, { stock: product.stock - item.quantity });
+      }
     }
-
     if (insufficientStock.length > 0) {
-        return { status: 'error', message: 'Insufficient stock for some products', insufficientStock };
+      return { status: 'error', message: 'Stock insuficiente para algunos productos', insufficientStock };
     }
-
     const ticket = await TicketService.createTicket({
-        amount: totalAmount,
-        purchaser: cart.user.email,
-        products: cart.products
+      amount: totalAmount,
+      purchaser: cart.user.email,
+      products: cart.products
     });
-
     await CartService.deleteAllProducts(cid);
-
     return ticket;
-};
+  };
